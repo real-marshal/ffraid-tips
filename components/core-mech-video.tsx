2b2f59f3a88d6be4b07833b1f17532c96fd5f376
Real_Marshal <remarshal@protonmail.com>
2025-04-05 02:43:11 +0300

    add deferred video loading and out-of-view pausing

--- components/core-mech-video.tsx
+++ components/core-mech-video.tsx
@@ -1,3 +1,8 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+import { useInView } from 'react-intersection-observer'
+
 export function CoreMechVideo({
   duty,
   filename,
@@ -7,16 +12,52 @@ export function CoreMechVideo({
   filename: string
   note?: string
 }) {
+  const ref = useRef<HTMLVideoElement>(null)
+  const { inView, ref: wrapperRef } = useInView({
+    onChange: (inView) => {
+      if (!ref.current) return
+
+      if (inView) {
+        if (ref.current.readyState !== HTMLMediaElement.HAVE_ENOUGH_DATA) {
+          console.log(`loading ${ref.current.currentSrc}`)
+          ref.current.load()
+        } else {
+          console.log(`playing ${ref.current.currentSrc}`)
+          void ref.current.play()
+        }
+      } else {
+        console.log(`pausing ${ref.current.currentSrc}`)
+        ref.current.pause()
+      }
+    },
+  })
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (ref.current && !ref.current.autoplay) {
+        console.log(`deferred loading of ${ref.current.currentSrc}`)
+        ref.current.load()
+      }
+    }, 3000)
+  }, [])
+
   return (
-    <div>
+    <div ref={wrapperRef}>
       <video
+        ref={ref}
         tabIndex={0}
-        autoPlay
         muted
         loop
-        preload='none'
+        preload='metadata'
         playsInline
         className='rounded-md focus:scale-[1.2] focus:rounded-md transition duration-300 ease-out cursor-pointer'
+        onLoadedData={() => {
+          if (!ref.current) return
+
+          if (inView) {
+            void ref.current.play()
+          }
+        }}
       >
         <source src={`./duties/${duty}/av1/${filename}`} type='video/webm;codecs="av01.0.05M.08"' />
         <source src={`./duties/${duty}/vp9/${filename}`} type='video/webm;codecs="vp9"' />
