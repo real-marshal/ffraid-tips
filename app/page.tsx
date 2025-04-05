701908101e79146e4a5e5010ac2da01be34c815f
Real_Marshal <remarshal@protonmail.com>
2025-04-05 03:48:25 +0300

    add some main page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className='min-h-screen'>
-      <p>main</p>
+    <main className='min-h-screen text-xl p-10'>
+      <h3 className='italic'>This is still a work-in-progress project</h3>
+      <p>April 2 - EX4 guide released</p>
+      <p>April 5-6 (very soon) - planned M5S guide release</p>
     </main>
   )
 }
